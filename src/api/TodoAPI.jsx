02e208ab4cae999b1f41157b9f77466c02576a5f
Jaimin Darji <jaimin.d.88@gmail.com>
2016-12-09 22:14:30 -0500

    added search logic

--- src/api/TodoAPI.jsx
+++ src/api/TodoAPI.jsx
@@ -31,6 +31,22 @@ module.exports = {
         });
 
         // filter by search text
+        filtered = filtered.filter((todo)=>{
+            var text = todo.text.toLowerCase();
+            return text.length === 0 || text.indexOf(search) > -1;
+        });
+
+
+        // sort the arrays by completed
+        filtered.sort((a, b)=>{
+            if (!a.completed && b.completed){
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
 
         return filtered;
     }
