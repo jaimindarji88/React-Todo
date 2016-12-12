d0d280cdbecad4583484a714eaa5bb6b89cfd827
Jaimin Darji <jaimin.d.88@gmail.com>
2016-12-11 23:21:40 -0500

    added todo reducer

--- src/reducers/reducers.jsx
+++ src/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+import uuid from 'uuid'
+import moment from 'moment'
+
 export var searchTextReducer = (state='', action) => {
     switch (action.type){
         case "SET_SEARCH_TEXT":
@@ -14,4 +17,35 @@ export var showCompletedReducer = (state=false, action) => {
         default:
             return state;
     }
-}
+};
+
+export var todosReducer = (state=[], action) => {
+    switch (action.type){
+        case "ADD_TODO":
+            return [
+                ...state,
+                {
+                    id: uuid(),
+                    text: text,
+                    completed: false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ];
+        case "TOGGLE_TODO":
+            return state.map((todo)=>{
+                if (todo.id === action.id) {
+                    var next = !todo.completed;
+
+                    return {
+                        ...todo,
+                        completed: next,
+                        completedAt: next ? moment.unix() : undefined
+
+                    }
+                }
+            });
+        default:
+            return state;
+    }
+};
